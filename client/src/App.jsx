174f6450d6906ec61652867befcf94cede908dbc
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 14:52:47 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Banner from "./components/Banner";
 import ProductGrid from "./components/ProductGrid";
+import BottomNav from "./components/BottomNav";
 
-export default function App() {
+function App() {
   return (
     <div className="min-h-screen bg-background pb-24">
       <Header />
       <Categories />
       <Banner />
       <ProductGrid />
+      <BottomNav />
     </div>
   );
 }
+
+export default App;
