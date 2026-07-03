0ada89363f30699fe54ba947d5b241758ca485b6
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 15:46:24 +0500

    Create banners.js

---
+++ client/src/data/banners.js
@@ -0,0 +1,22 @@
+const banners = [
+  {
+    id: 1,
+    title: "30% gacha chegirma",
+    subtitle: "Meva va sabzavotlar",
+    color: "from-green-500 to-green-700"
+  },
+  {
+    id: 2,
+    title: "Yangi mahsulotlar",
+    subtitle: "Har kuni yangilanadi",
+    color: "from-orange-500 to-red-500"
+  },
+  {
+    id: 3,
+    title: "Bepul yetkazib berish",
+    subtitle: "100 000 so'mdan yuqori",
+    color: "from-blue-500 to-cyan-500"
+  }
+];
+
+export default banners;
