5511acc0e9a84219a4c4ee10ac636462f5796ca2
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 15:30:15 +0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import { CartProvider } from "./context/CartContext";
+import { WishlistProvider } from "./context/WishlistContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <WishlistProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </WishlistProvider>
   </React.StrictMode>
 );
