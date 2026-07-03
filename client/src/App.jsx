ce88ebfa9a9f29c9e5f89ed9b78b6481681c6527
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 14:44:26 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Categories from "./components/Categories";
+import Banner from "./components/Banner";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background pb-24">
       <Header />
       <Categories />
+      <Banner />
     </div>
   );
 }
