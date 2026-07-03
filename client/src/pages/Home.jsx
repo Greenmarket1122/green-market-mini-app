eb933111f3a58026cb002efa99d4d4ea7247a5a6
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 15:42:13 +0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,45 @@
 import Header from "../components/Header";
-import Categories from "../components/Categories";
+import SearchBar from "../components/SearchBar";
+import CategoryFilter from "../components/CategoryFilter";
 import Banner from "../components/Banner";
 import ProductGrid from "../components/ProductGrid";
 import BottomNav from "../components/BottomNav";
 
+import useProducts from "../hooks/useProducts";
+
 export default function Home() {
+
+  const {
+    products,
+    search,
+    setSearch,
+    category,
+    setCategory,
+  } = useProducts();
+
   return (
     <div className="min-h-screen bg-background pb-24">
+
       <Header />
-      <Categories />
+
+      <SearchBar
+        value={search}
+        onChange={setSearch}
+      />
+
+      <CategoryFilter
+        selected={category}
+        onSelect={setCategory}
+      />
+
       <Banner />
-      <ProductGrid />
+
+      <ProductGrid
+        products={products}
+      />
+
       <BottomNav />
+
     </div>
   );
 }
