01654e7001f105a64ed36fdc56b5e71085435955
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 15:08:47 +0500

    Create Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Categories from "../components/Categories";
+import Banner from "../components/Banner";
+import ProductGrid from "../components/ProductGrid";
+import BottomNav from "../components/BottomNav";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background pb-24">
+      <Header />
+      <Categories />
+      <Banner />
+      <ProductGrid />
+      <BottomNav />
+    </div>
+  );
+}
