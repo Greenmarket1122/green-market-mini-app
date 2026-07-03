72e42e1684143a6cc7ca867d9b1009388d876f6b
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 14:43:14 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,63 +1,11 @@
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+
 export default function App() {
-  const products = [
-    {
-      id: 1,
-      name: "Monster Energy",
-      price: "30 000 UZS",
-      image: "https://picsum.photos/300?1"
-    },
-    {
-      id: 2,
-      name: "Coca Cola",
-      price: "14 000 UZS",
-      image: "https://picsum.photos/300?2"
-    }
-  ];
-
   return (
-    <div className="app">
-
-      <header className="header">
-        <h2>🥬 Green Market</h2>
-
-        <input
-          className="search"
-          placeholder="Mahsulotlarni qidirish..."
-        />
-      </header>
-
-      <div className="categories">
-        <button>🥤 Ichimliklar</button>
-        <button>🍫 Shirinliklar</button>
-        <button>🥬 Sabzavotlar</button>
-        <button>🍎 Mevalar</button>
-      </div>
-
-      <div className="banner">
-        <h2>Chegirmalar</h2>
-        <p>Bugungi aksiyalarni o'tkazib yubormang!</p>
-      </div>
-
-      <div className="products">
-
-        {products.map((item) => (
-
-          <div className="card" key={item.id}>
-
-            <img src={item.image} />
-
-            <h3>{item.name}</h3>
-
-            <p>{item.price}</p>
-
-            <button>Savatga</button>
-
-          </div>
-
-        ))}
-
-      </div>
-
+    <div className="min-h-screen bg-background">
+      <Header />
+      <Categories />
     </div>
   );
 }
