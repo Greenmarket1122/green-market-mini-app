6095b6bef1664a61d36f1a9cf4d145b997f8d8b5
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 15:41:44 +0500

    Update ProductGrid.jsx

--- client/src/components/ProductGrid.jsx
+++ client/src/components/ProductGrid.jsx
@@ -1,37 +1,13 @@
 import ProductCard from "./ProductCard";
 
-const products = [
-  {
-    id: 1,
-    name: "Coca Cola 1.5L",
-    price: "14 990 so'm",
-    image: "https://picsum.photos/300?1"
-  },
-  {
-    id: 2,
-    name: "Olma",
-    price: "18 500 so'm",
-    image: "https://picsum.photos/300?2"
-  },
-  {
-    id: 3,
-    name: "Banan",
-    price: "27 900 so'm",
-    image: "https://picsum.photos/300?3"
-  },
-  {
-    id: 4,
-    name: "Sut 1L",
-    price: "16 500 so'm",
-    image: "https://picsum.photos/300?4"
-  }
-];
-
-export default function ProductGrid() {
+export default function ProductGrid({ products }) {
   return (
     <div className="grid grid-cols-2 gap-4 p-4">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
       ))}
     </div>
   );
