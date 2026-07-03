eb917ceaa18b7de5dc92087c72cb6a73c98907bd
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 15:28:08 +0500

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,15 +1,46 @@
+import { useCart } from "../context/CartContext";
+import CartItem from "../components/CartItem";
+import CartSummary from "../components/CartSummary";
 import BottomNav from "../components/BottomNav";
 
 export default function Cart() {
-  return (
-    <div className="min-h-screen bg-background pb-24">
-      <h1 className="text-2xl font-bold p-4">🛒 Savatcha</h1>
+  const { cart } = useCart();
 
-      <div className="p-4 text-gray-500">
-        Savatcha hozircha bo'sh.
+  const total = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+
+  return (
+    <div className="min-h-screen bg-background pb-52">
+
+      <h1 className="text-2xl font-bold p-4">
+        🛒 Savatcha
+      </h1>
+
+      <div className="px-4">
+
+        {cart.length === 0 ? (
+          <div className="text-center text-gray-500 mt-20">
+            Savatchangiz bo'sh
+          </div>
+        ) : (
+          cart.map((item) => (
+            <CartItem
+              key={item.id}
+              item={item}
+            />
+          ))
+        )}
+
       </div>
 
+      {cart.length > 0 && (
+        <CartSummary total={total} />
+      )}
+
       <BottomNav />
+
     </div>
   );
 }
