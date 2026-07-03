fbf9b092c9562a3e70100693ba4b94b3825b35bc
Greenmarket1122 <kurort289@gmail.com>
2026-07-03 14:55:31 +0500

    Create products.js

---
+++ client/src/data/products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 1,
+    name: "Coca Cola 1.5L",
+    price: 14990,
+    image: "https://picsum.photos/300?1",
+    category: "Ichimliklar",
+    discount: 10,
+    rating: 4.8,
+    stock: true
+  },
+  {
+    id: 2,
+    name: "Olma Golden",
+    price: 18990,
+    image: "https://picsum.photos/300?2",
+    category: "Mevalar",
+    discount: 0,
+    rating: 4.9,
+    stock: true
+  },
+  {
+    id: 3,
+    name: "Banan",
+    price: 27990,
+    image: "https://picsum.photos/300?3",
+    category: "Mevalar",
+    discount: 5,
+    rating: 4.7,
+    stock: true
+  },
+  {
+    id: 4,
+    name: "Sut 1L",
+    price: 16990,
+    image: "https://picsum.photos/300?4",
+    category: "Sut",
+    discount: 0,
+    rating: 4.6,
+    stock: true
+  }
+];
+
+export default products;
